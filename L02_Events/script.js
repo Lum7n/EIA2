7c8e20ba632b44876bd479f0257c2129933ced70
Lum7n <56406099+Lum7n@users.noreply.github.com>
2020-05-03 04:32:47 +0200

    Add files via upload

--- L02_Events/script.js
+++ L02_Events/script.js
@@ -1,1 +1,40 @@
+var currentSeq;
+var showshuffled;
+var timeout;
+window.addEventListener("load", function () {
+    document.getElementById("input").addEventListener("keydown", help);
+    document.getElementById("card").addEventListener("click", moveCards);
+    document.getElementById("chooseSequence").addEventListener("click", choosenSeq);
+    document.getElementById("sequence").addEventListener("change", getSequence);
+    document.getElementById("start").addEventListener("click", start);
+    document.getElementById("***???***").addEventListener("load", handleload);
+});
+function getSequence() {
+    currentSeq = [document.getElementById("sequence").value];
+}
+function choosenSeq() {
+    currentSeq = [document.getElementById("chooseSequence").textContent]; //innerText wär auch möglich
+    //Listener ist auf parent gelegt, wie identifiziere ich das angeklickte child
+    console.log(currentSeq);
+}
+function handleload() {
+}
+function start() {
+}
+function moveCards() {
+    console.log("keine Ahnung");
+}
+function help() {
+    var x = event.key;
+    console.log(x);
+    var y = document.getElementById("card").innerText;
+    //wie durchsucht er alle Elemente mit der ID "cards" nach dem passenden Buchstaben?
+    if (x == y) {
+        document.getElementById("card").style.backgroundColor = "white";
+    }
+}
+function timeup() {
+    document.getElementById("card").style.backgroundColor = "black";
+    setTimeout(function () { alert("times up"); }, timeout);
+}
 //# sourceMappingURL=script.js.map
--- L02_Events/script.ts
+++ L02_Events/script.ts
@@ -1,1 +1,49 @@
+let currentSeq: string[];
+let showshuffled: number;
+let timeout: number;
 
+window.addEventListener("load", function (): void {
+    document.getElementById("input").addEventListener("keydown", help);
+    document.getElementById("card").addEventListener("click", moveCards);
+    document.getElementById("chooseSequence").addEventListener("click", choosenSeq);
+    document.getElementById("sequence").addEventListener("change", getSequence);
+    document.getElementById("start").addEventListener("click", start);
+    document.getElementById("***???***").addEventListener("load", handleload);
+});
+
+function getSequence(): void {
+    currentSeq = [document.getElementById("sequence").value];
+}
+
+function choosenSeq(): void {
+    currentSeq = [document.getElementById("chooseSequence").textContent]; //innerText wär auch möglich
+    //Listener ist auf parent gelegt, wie identifiziere ich das angeklickte child
+    console.log(currentSeq);
+}
+
+function handleload(): void {
+
+}
+
+function start(): void {
+
+}
+
+function moveCards(): void {
+    console.log("keine Ahnung");
+}
+
+function help(): void {
+    var x: string = event.key;
+    console.log(x);
+    var y: string = document.getElementById("card").innerText;
+    //wie durchsucht er alle Elemente mit der ID "cards" nach dem passenden Buchstaben?
+    if (x == y) {
+        document.getElementById("card").style.backgroundColor = "white";
+    }
+}
+
+function timeup(): void {
+    document.getElementById("card").style.backgroundColor = "black";
+    setTimeout(function (): void { alert("times up"); }, timeout);
+}
