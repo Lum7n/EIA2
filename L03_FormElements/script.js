bb82f6110c2af0c47ce3ff811ac9de7b46cef276
Lum7n <56406099+Lum7n@users.noreply.github.com>
2020-05-10 04:29:50 +0200

    Add files via upload

--- L03_FormElements/script.js
+++ L03_FormElements/script.js
@@ -1,20 +1,69 @@
+"use strict";
 window.addEventListener("load", init);
 function init(_event) {
-    console.log("Init");
-    var divs = document.querySelectorAll("div");
-    // Install listeners on divs
-    for (var i = 0; i < divs.length; i++) {
-        var div = divs[i];
-        div.addEventListener("change", handleChange);
-        div.addEventListener("input", handleChange);
+    let lineE = document.getElementById("ArtikelIn");
+    lineE.addEventListener("click", createLineE);
+    let lineB = document.getElementById("BankIn");
+    lineB.addEventListener("click", createLineB);
+    let lineH = document.getElementById("WorkIn");
+    lineH.addEventListener("click", createLineH);
+    let lineA = document.getElementById("DiffIn");
+    lineA.addEventListener("click", createLineA);
+    let setW = document.getElementById("getWhite");
+    setW.addEventListener("click", setWhite);
+    let setM = document.getElementById("getMain");
+    setM.addEventListener("click", setMain);
+    let slider = document.getElementById("Slider");
+    slider.addEventListener("input", displayAmount);
+    let formEinkaufen = document.querySelector("div#Einkaufen");
+    formEinkaufen.addEventListener("change", handleChangeE);
+    // let formBank: HTMLDivElement = <HTMLDivElement>document.querySelector("div#Bank");
+    // formBank.addEventListener("change", handleChangeB);
+    // let formHaushalt: HTMLDivElement = <HTMLDivElement>document.querySelector("div#Haushalt");
+    // formHaushalt.addEventListener("change", handleChangeH);
+    // let formAnderes: HTMLDivElement = <HTMLDivElement>document.querySelector("div#Anderes");
+    // formAnderes.addEventListener("change", handleChangeA);
+}
+function handleChangeE() {
+    let tableE = document.querySelector("div#tableE td");
+    tableE.innerHTML = "";
+    let formData = new FormData(document.forms[0]);
+    for (let entry of formData) {
+        let item = document.querySelector("[value='" + entry[1] + "']");
+        let price = Number(item.getAttribute("price"));
+        tableE.innerHTML += item.name + "  € " + price;
     }
 }
-function handleChange(_event) {
-    var target = _event.target;
+function displayAmount(_event) {
+    let target = _event.target;
     // Amount response
     if (target.name == "Slider") {
-        var ouput = document.querySelector("output");
+        let ouput = document.querySelector("output");
         ouput.value = target.value + " €";
     }
+}
+function createLineE() {
+    console.log("e");
+}
+function createLineB() {
+    console.log("b");
+}
+function createLineH() {
+    console.log("h");
+}
+function createLineA() {
+    console.log("a");
+}
+function setWhite() {
+    var m = document.getElementById("main");
+    m.style.display = "none";
+    var w = document.getElementById("whiteCover");
+    w.style.display = "contents";
+}
+function setMain() {
+    var m = document.getElementById("whiteCover");
+    m.style.display = "none";
+    var w = document.getElementById("main");
+    w.style.display = "contents";
 }
 //# sourceMappingURL=script.js.map
--- L03_FormElements/script.ts
+++ L03_FormElements/script.ts
@@ -1,23 +1,96 @@
-    window.addEventListener("load", init);
+window.addEventListener("load", init);
 
-    function init(_event: Event): void {
-        console.log("Init");
-        let divs: NodeListOf<HTMLDivElement> = document.querySelectorAll("div");
+function init(_event: Event): void {
 
-        // Install listeners on divs
-        for (let i: number = 0; i < divs.length; i++) {
-            let div: HTMLDivElement = divs[i];
-            div.addEventListener("change", handleChange);
-            div.addEventListener("input", handleChange);
-        }
+    let lineE: HTMLElement = <HTMLElement>document.getElementById("ArtikelIn");
+    lineE.addEventListener("click", createLineE);
+
+    let lineB: HTMLElement = <HTMLElement>document.getElementById("BankIn");
+    lineB.addEventListener("click", createLineB);
+
+    let lineH: HTMLElement = <HTMLElement>document.getElementById("WorkIn");
+    lineH.addEventListener("click", createLineH);
+
+    let lineA: HTMLElement = <HTMLElement>document.getElementById("DiffIn");
+    lineA.addEventListener("click", createLineA);
+
+
+    let setW: HTMLElement = <HTMLElement>document.getElementById("getWhite");
+    setW.addEventListener("click", setWhite);
+
+    let setM: HTMLElement = <HTMLElement>document.getElementById("getMain");
+    setM.addEventListener("click", setMain);
+
+
+    let slider: HTMLElement = <HTMLElement>document.getElementById("Slider");
+    slider.addEventListener("input", displayAmount);
+
+    let formEinkaufen: HTMLDivElement = <HTMLDivElement>document.querySelector("div#Einkaufen");
+    formEinkaufen.addEventListener("change", handleChangeE);
+
+    // let formBank: HTMLDivElement = <HTMLDivElement>document.querySelector("div#Bank");
+    // formBank.addEventListener("change", handleChangeB);
+
+    // let formHaushalt: HTMLDivElement = <HTMLDivElement>document.querySelector("div#Haushalt");
+    // formHaushalt.addEventListener("change", handleChangeH);
+
+    // let formAnderes: HTMLDivElement = <HTMLDivElement>document.querySelector("div#Anderes");
+    // formAnderes.addEventListener("change", handleChangeA);
+}
+
+function handleChangeE(): void {
+
+    let tableE: HTMLTableDataCellElement = <HTMLTableDataCellElement>document.querySelector("div#tableE td");
+    tableE.innerHTML = "";
+
+    let formData: FormData = new FormData(document.forms[0]);
+    for (let entry of formData) {
+        let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
+        let price: number = Number(item.getAttribute("price"));
+
+        tableE.innerHTML += item.name + "  € " + price;
     }
+}
 
-    function handleChange(_event: Event): void {
-        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+function displayAmount(_event: Event): void {
+    let target: HTMLInputElement = <HTMLInputElement>_event.target;
 
-        // Amount response
-        if (target.name == "Slider") {
-            let ouput: HTMLOutputElement = <HTMLOutputElement>document.querySelector("output");
-            ouput.value = target.value + " €";
-        }
+    // Amount response
+    if (target.name == "Slider") {
+        let ouput: HTMLOutputElement = <HTMLOutputElement>document.querySelector("output");
+        ouput.value = target.value + " €";
     }
+}
+
+function createLineE(): void {
+    console.log("e");
+}
+
+function createLineB(): void {
+    console.log("b");
+
+}
+
+function createLineH(): void {
+    console.log("h");
+
+}
+
+function createLineA(): void {
+    console.log("a");
+
+}
+
+function setWhite(): void {
+    var m: HTMLElement = <HTMLElement>document.getElementById("main");
+    m.style.display = "none";
+    var w: HTMLElement = <HTMLElement>document.getElementById("whiteCover");
+    w.style.display = "contents";
+}
+
+function setMain(): void {
+    var m: HTMLElement = <HTMLElement>document.getElementById("whiteCover");
+    m.style.display = "none";
+    var w: HTMLElement = <HTMLElement>document.getElementById("main");
+    w.style.display = "contents";
+}
