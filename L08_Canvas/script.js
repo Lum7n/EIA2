32cd2b262cd8ca3392bbc6d3ffa11d9e06e518bd
Lum7n <56406099+Lum7n@users.noreply.github.com>
2020-06-14 04:12:19 +0200

    Add files via upload

--- L08_Canvas/script.js
+++ L08_Canvas/script.js
@@ -1,6 +1,6 @@
 "use strict";
-var CanvasVirus;
-(function (CanvasVirus) {
+var CanvasVirusus;
+(function (CanvasVirusus) {
     let crc2;
     window.addEventListener("load", init);
     function init(_event) {
@@ -10,8 +10,8 @@ var CanvasVirus;
             if (crc2) {
                 var imgBig = document.getElementById("background");
                 crc2.drawImage(imgBig, 0, 0, 800, 600);
-                cell();
-                virus();
+                cells();
+                viruses();
             }
         }
         let canvasSmall = document.querySelector("canvas#screenSmall");
@@ -20,25 +20,25 @@ var CanvasVirus;
             if (crc2) {
                 var imgSmall = document.getElementById("background");
                 crc2.drawImage(imgSmall, 0, 0, 800, 600);
-                cell();
-                virus();
+                cells();
+                viruses();
             }
         }
     }
-    function cell() {
+    function cells() {
         if (crc2) {
             crc2.beginPath();
             crc2.strokeStyle = "#9c214c";
             crc2.fillStyle = "#ffd1e1";
             crc2.ellipse(280, 56, 60, 35, 10, 0.8, 1.3 * Math.PI);
-            crc2.ellipse(225, 50, 60, 35, 24.8, 2.1, 1.7 * Math.PI);
+            crc2.ellipse(225, 50, 60, 35, 24.8, 2.3, 1.7 * Math.PI);
             crc2.fill();
             crc2.closePath();
             crc2.stroke();
             let cell = new Path2D();
             crc2.rotate(-25 * Math.PI / 180);
             cell.ellipse(-50, 456, 60, 35, 10, 0.8, 1.3 * Math.PI);
-            cell.ellipse(-105, 450, 60, 35, 24.8, 2.1, 1.7 * Math.PI);
+            cell.ellipse(-105, 450, 60, 35, 24.8, 2.3, 1.7 * Math.PI);
             cell.closePath();
             crc2.fill(cell);
             crc2.stroke(cell);
@@ -50,23 +50,23 @@ var CanvasVirus;
             crc2.restore();
         }
     }
-    function virus() {
+    function viruses() {
         if (crc2) {
-            let vir = new Path2D();
+            let virus = new Path2D();
             crc2.strokeStyle = "#178f2b";
             crc2.fillStyle = "#b3f576";
             crc2.lineWidth = 3;
-            vir.lineTo(335, 400);
-            vir.lineTo(265, 400);
-            vir.moveTo(300, 400);
-            vir.lineTo(317, 365);
-            vir.lineTo(286, 432);
-            vir.moveTo(300, 400);
-            vir.lineTo(320, 430);
-            vir.lineTo(280, 368);
-            crc2.fill(vir);
-            vir.closePath();
-            crc2.stroke(vir);
+            virus.lineTo(335, 400);
+            virus.lineTo(265, 400);
+            virus.moveTo(300, 400);
+            virus.lineTo(317, 365);
+            virus.lineTo(286, 432);
+            virus.moveTo(300, 400);
+            virus.lineTo(320, 430);
+            virus.lineTo(280, 368);
+            crc2.fill(virus);
+            virus.closePath();
+            crc2.stroke(virus);
             crc2.beginPath();
             crc2.lineWidth = 2;
             crc2.arc(300, 400, 20, 0, 2 * Math.PI);
@@ -75,7 +75,7 @@ var CanvasVirus;
             crc2.stroke();
             crc2.lineWidth = 3;
             crc2.translate(50, -50);
-            crc2.stroke(vir);
+            crc2.stroke(virus);
             crc2.beginPath();
             crc2.lineWidth = 2;
             crc2.arc(300, 400, 20, 0, 2 * Math.PI);
@@ -84,7 +84,7 @@ var CanvasVirus;
             crc2.stroke();
             crc2.lineWidth = 3;
             crc2.translate(-150, -50);
-            crc2.stroke(vir);
+            crc2.stroke(virus);
             crc2.beginPath();
             crc2.lineWidth = 2;
             crc2.arc(300, 400, 20, 0, 2 * Math.PI);
@@ -93,7 +93,7 @@ var CanvasVirus;
             crc2.stroke();
             crc2.lineWidth = 3;
             crc2.translate(50, 250);
-            crc2.stroke(vir);
+            crc2.stroke(virus);
             crc2.beginPath();
             crc2.lineWidth = 2;
             crc2.arc(300, 400, 20, 0, 2 * Math.PI);
@@ -102,7 +102,7 @@ var CanvasVirus;
             crc2.stroke();
             crc2.lineWidth = 3;
             crc2.translate(-100, 20);
-            crc2.stroke(vir);
+            crc2.stroke(virus);
             crc2.beginPath();
             crc2.lineWidth = 2;
             crc2.arc(300, 400, 20, 0, 2 * Math.PI);
@@ -111,7 +111,7 @@ var CanvasVirus;
             crc2.stroke();
             crc2.lineWidth = 3;
             crc2.translate(-150, -200);
-            crc2.stroke(vir);
+            crc2.stroke(virus);
             crc2.beginPath();
             crc2.lineWidth = 2;
             crc2.arc(300, 400, 20, 0, 2 * Math.PI);
@@ -120,7 +120,7 @@ var CanvasVirus;
             crc2.stroke();
         }
     }
-})(CanvasVirus || (CanvasVirus = {}));
+})(CanvasVirusus || (CanvasVirusus = {}));
 if (crc2) {
     crc2.beginPath();
     crc2.strokeStyle = "#FFFFFFF";
--- L08_Canvas/script.ts
+++ L08_Canvas/script.ts
@@ -1,4 +1,4 @@
-namespace CanvasVirus {
+namespace CanvasVirusus {
 
     let crc2: CanvasRenderingContext2D | null;
 
@@ -15,9 +15,9 @@ namespace CanvasVirus {
                 var imgBig: CanvasImageSource = <CanvasImageSource>document.getElementById("background");
                 crc2.drawImage(imgBig, 0, 0, 800, 600);
 
-                cell();
+                cells();
 
-                virus();
+                viruses();
             }
         }
 
@@ -30,21 +30,21 @@ namespace CanvasVirus {
                 var imgSmall: CanvasImageSource = <CanvasImageSource>document.getElementById("background");
                 crc2.drawImage(imgSmall, 0, 0, 800, 600);
 
-                cell();
+                cells();
 
-                virus();
+                viruses();
             }
         }
     }
 
-    function cell(): void {
+    function cells(): void {
 
         if (crc2) {
             crc2.beginPath();
             crc2.strokeStyle = "#9c214c";
             crc2.fillStyle = "#ffd1e1";
             crc2.ellipse(280, 56, 60, 35, 10, 0.8, 1.3 * Math.PI);
-            crc2.ellipse(225, 50, 60, 35, 24.8, 2.1, 1.7 * Math.PI);
+            crc2.ellipse(225, 50, 60, 35, 24.8, 2.3, 1.7 * Math.PI);
             crc2.fill();
             crc2.closePath();
             crc2.stroke();
@@ -52,7 +52,7 @@ namespace CanvasVirus {
             let cell: Path2D = new Path2D();
             crc2.rotate(-25 * Math.PI / 180);
             cell.ellipse(-50, 456, 60, 35, 10, 0.8, 1.3 * Math.PI);
-            cell.ellipse(-105, 450, 60, 35, 24.8, 2.1, 1.7 * Math.PI);
+            cell.ellipse(-105, 450, 60, 35, 24.8, 2.3, 1.7 * Math.PI);
             cell.closePath();
             crc2.fill(cell);
             crc2.stroke(cell);
@@ -69,23 +69,23 @@ namespace CanvasVirus {
 
     }
 
-    function virus(): void {
+    function viruses(): void {
         if (crc2) {
-            let vir: Path2D = new Path2D();
+            let virus: Path2D = new Path2D();
             crc2.strokeStyle = "#178f2b";
             crc2.fillStyle = "#b3f576";
             crc2.lineWidth = 3;
-            vir.lineTo(335, 400);
-            vir.lineTo(265, 400);
-            vir.moveTo(300, 400);
-            vir.lineTo(317, 365);
-            vir.lineTo(286, 432);
-            vir.moveTo(300, 400);
-            vir.lineTo(320, 430);
-            vir.lineTo(280, 368);
-            crc2.fill(vir);
-            vir.closePath();
-            crc2.stroke(vir);
+            virus.lineTo(335, 400);
+            virus.lineTo(265, 400);
+            virus.moveTo(300, 400);
+            virus.lineTo(317, 365);
+            virus.lineTo(286, 432);
+            virus.moveTo(300, 400);
+            virus.lineTo(320, 430);
+            virus.lineTo(280, 368);
+            crc2.fill(virus);
+            virus.closePath();
+            crc2.stroke(virus);
 
             crc2.beginPath();
             crc2.lineWidth = 2;
@@ -96,7 +96,7 @@ namespace CanvasVirus {
 
             crc2.lineWidth = 3;
             crc2.translate(50, -50);
-            crc2.stroke(vir);
+            crc2.stroke(virus);
 
             crc2.beginPath();
             crc2.lineWidth = 2;
@@ -107,7 +107,7 @@ namespace CanvasVirus {
 
             crc2.lineWidth = 3;
             crc2.translate(-150, -50);
-            crc2.stroke(vir);
+            crc2.stroke(virus);
 
             crc2.beginPath();
             crc2.lineWidth = 2;
@@ -118,7 +118,7 @@ namespace CanvasVirus {
 
             crc2.lineWidth = 3;
             crc2.translate(50, 250);
-            crc2.stroke(vir);
+            crc2.stroke(virus);
 
             crc2.beginPath();
             crc2.lineWidth = 2;
@@ -129,7 +129,7 @@ namespace CanvasVirus {
 
             crc2.lineWidth = 3;
             crc2.translate(-100, 20);
-            crc2.stroke(vir);
+            crc2.stroke(virus);
 
             crc2.beginPath();
             crc2.lineWidth = 2;
@@ -140,7 +140,7 @@ namespace CanvasVirus {
 
             crc2.lineWidth = 3;
             crc2.translate(-150, -200);
-            crc2.stroke(vir);
+            crc2.stroke(virus);
 
             crc2.beginPath();
             crc2.lineWidth = 2;
